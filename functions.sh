0f1f6d68a6e30b008c51e772c2ca88786ddddd39
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-04-19 15:07:48 -0400

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -479,7 +479,7 @@ microk8sStart() {
 
 microk8sEnableBasic() {
   printInfoSection "Enable DNS, Storage, NGINX Ingress"
-  bashas 'microk8s.enable dns:192.168.3.27'
+  bashas 'microk8s.enable dns'
   waitForAllPods
   bashas 'microk8s.enable storage'
   waitForAllPods
