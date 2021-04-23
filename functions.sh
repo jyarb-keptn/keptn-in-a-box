e605f1fa670c094b092ae76155c6b56ba1d04dc2
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-04-23 13:07:24 -0400

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -8,7 +8,7 @@
 # ==================================================
 #      ----- Components Versions -----             #
 # ==================================================
-KIAB_RELEASE="main"
+KIAB_RELEASE="0.8.2"
 ISTIO_VERSION=1.9.1
 CERTMANAGER_VERSION=0.14.0
 # https://github.com/keptn/keptn
@@ -19,7 +19,7 @@ KEPTN_DT_SERVICE_VERSION=0.13.0
 KEPTN_DT_SLI_SERVICE_VERSION=0.10.0
 # https://github.com/keptn/examples
 KEPTN_EXAMPLES_REPO="https://github.com/keptn/examples.git"
-KEPTN_EXAMPLES_BRANCH="release-0.8.1"
+KEPTN_EXAMPLES_BRANCH="release-0.8.2"
 KEPTN_EXAMPLES_DIR="~/examples"
 KEPTN_CATALOG_REPO="https://github.com/jyarb-keptn/overview.git"
 KEPTN_CATALOG_BRANCH="master"
