86ce742835c9ec5437d31349e95427b9a8b41d15
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-04-26 12:25:24 -0400

    Update buildjenkins.sh

--- resources/jenkins/build/buildjenkins.sh
+++ resources/jenkins/build/buildjenkins.sh
@@ -3,8 +3,12 @@
 # Pull docker image
 docker pull jenkins/jenkins:2.288
 
-# we build the image and tag it
-docker build -t pcjeffmac/jenkins:2.288 .
+docker images
+
+# create personal docker image
+docker tag jenkins/jenkins:2.288 pcjeffmac/jenkins:2.288
+
+docker images
 
 # Pushit to dockerhub
 docker push pcjeffmac/jenkins:2.288
