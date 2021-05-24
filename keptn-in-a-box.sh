5e4a8ec796779891d0576bdb419f93b52c93965b
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-05-24 10:11:02 -0400

    Update keptn-in-a-box.sh

--- keptn-in-a-box.sh
+++ keptn-in-a-box.sh
@@ -123,7 +123,7 @@ DOMAIN="`curl http://checkip.amazonaws.com`.nip.io"
 #      ----- Functions Location -----              #
 # ==================================================
 # - Keptn in a Box release
-KIAB_RELEASE="main"
+KIAB_RELEASE="0.8.3"
 # - Functions file location
 FUNCTIONS_FILE_REPO="https://raw.githubusercontent.com/jyarb-keptn/keptn-in-a-box/${KIAB_RELEASE}/functions.sh"
 
