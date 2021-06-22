dcdb4e9d8355afda1626127cab8158f41961834c
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-06-22 09:02:55 -0400

    Update functions.sh

--- functions.sh
+++ functions.sh
@@ -14,12 +14,12 @@ CERTMANAGER_VERSION=0.14.0
 # https://github.com/keptn/keptn
 KEPTN_VERSION=0.8.4
 # https://github.com/keptn-contrib/dynatrace-service
-KEPTN_DT_SERVICE_VERSION=0.13.1
+KEPTN_DT_SERVICE_VERSION=0.14.0
 # https://github.com/keptn-contrib/dynatrace-sli-service
 KEPTN_DT_SLI_SERVICE_VERSION=0.11.0
 # https://github.com/keptn/examples
 KEPTN_EXAMPLES_REPO="https://github.com/keptn/examples.git"
-KEPTN_EXAMPLES_BRANCH="release-0.8.3"
+KEPTN_EXAMPLES_BRANCH="release-0.8.4"
 KEPTN_EXAMPLES_DIR="~/examples"
 KEPTN_CATALOG_REPO="https://github.com/jyarb-keptn/overview.git"
 KEPTN_CATALOG_BRANCH="0.8.3"
