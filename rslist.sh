66b9e353087a68ba672790b16765941b7d072487
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-09-01 13:50:59 -0400

    Update rslist.sh

--- rslist.sh
+++ rslist.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# =======
+# clean up script for stale replica sets
+#
+
 rm rscleanup.sh
 
 kubectl get --all-namespaces rs -o json|jq -r '.items[] | select(.spec.replicas | contains(0)) | "kubectl delete rs --namespace=\(.metadata.namespace) \(.metadata.name)"' >> rscleanup.sh
