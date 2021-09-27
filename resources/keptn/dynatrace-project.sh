c2580b3f29eaadd33f3d0ed30f2f202b3b254d33
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-09-27 17:12:35 -0400

    Update dynatrace-project.sh

--- resources/keptn/dynatrace-project.sh
+++ resources/keptn/dynatrace-project.sh
@@ -3,3 +3,6 @@
     echo "load shipyard.yaml"
     keptn create project dynatrace --shipyard=./shipyard.yaml
     keptn add-resource --project=dynatrace --resource=./shipyard.yaml --resourceUri=shipyard.yaml
+    
+    keptn configure monitoring dynatrace --project=dynatrace
+    
