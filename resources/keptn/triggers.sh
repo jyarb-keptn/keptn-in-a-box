8adb64b8e9a941e98fb2d372084d82fee41246d5
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-09-28 10:55:21 -0400

    Update triggers.sh

--- resources/keptn/triggers.sh
+++ resources/keptn/triggers.sh
@@ -3,3 +3,5 @@
 keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=www --timeframe=30m --labels=buildId=3,executedBy=manual
 
 keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=Auth2 --timeframe=30m --labels=buildId=5,executedBy=manual
+
+keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=check --timeframe=30m --labels=buildId=5,executedBy=manual
