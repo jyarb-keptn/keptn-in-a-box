761ed51fbd2cac12ad4af195fe37a074fd917155
Jeff Yarbrough <pcjeffmac@gmail.com>
2021-10-27 15:06:14 -0400

    Update prepfiles.sh

--- resources/demo/prepfiles.sh
+++ resources/demo/prepfiles.sh
@@ -4,6 +4,10 @@ KEPTN_PREP_DIR="overview"
 KEPTN_EXAMPLES_DIR="examples"
 
 echo $HOME
+echo "moving files...."
+
+# copy new format for metric indicators
+cp ${HOME}/keptn-in-a-box/resources/demo/sli-config-dynatrace.yaml ${HOME}/${KEPTN_EXAMPLES_DIR}/onboarding-carts/sli-config-dynatrace.yaml
 
 #move files for jmeter
 cp ${HOME}/${KEPTN_PREP_DIR}/demo_onbording/dev/carts/jmeter/load.jmx ${HOME}/${KEPTN_EXAMPLES_DIR}/onboarding-carts/jmeter/load.jmx
