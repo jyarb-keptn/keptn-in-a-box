b018b43994e18fe34ef0ba357de13e782d2c2f5c
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-11-10 10:42:19 -0500

    Update setenv.sh

--- resources/dynatrace/setenv.sh
+++ resources/dynatrace/setenv.sh
@@ -4,10 +4,18 @@ source ../dynatrace/utils.sh
 readCredsFromFile
 printVariables
 
-echo $DT_TENANT
-echo $DT_API_TOKEN
+echo "Tenant: ${DT_TENANT}"
+echo "Toekn: ${DT_API_TOKEN}"
+
+KEPTN_DOMAIN=$1
+
+KEPTN_ENDPOINT=http://keptn.${KEPTN_DOMAIN}
+
+echo "keptn endpoint: ${KEPTN_ENDPOINT}"
 
 export DT_TENANT=$DT_TENANT
 export DT_API_TOKEN=$DT_API_TOKEN
 export DT_PAAS_TOKEN=$DT_PAAS_TOKEN
-export KEPTN_DOMAIN=$1
+export KEPTN_DOMAIN=${KEPTN_DOMAIN}
+export KEPTN_API_URL=${KEPTN_ENDPOINT}
+export KEPTN_BRIDGE_URL=${KEPTN_ENDPOINT}/bridge
