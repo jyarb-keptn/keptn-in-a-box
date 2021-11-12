0fcf34e53946d80dd784df8b52aa7fa9db3b4546
Jeff Yarbrough <pcjeffmac@gmail.com>
2021-11-12 08:33:15 -0500

    Update triggers.sh

--- resources/keptn/triggers.sh
+++ resources/keptn/triggers.sh
@@ -5,3 +5,5 @@ keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=easy
 keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=frontend --timeframe=30m --labels=buildId=5,executedBy=manual
 
 keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=check --timeframe=30m --labels=buildId=5,executedBy=manual
+
+keptn trigger evaluation --project=sockshop --stage=staging --service=carts --timeframe=30m --labels=buildId=5,executedBy=manual
