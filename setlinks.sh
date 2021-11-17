7dda8be9651ad1eedd5326d7edf3f6127d3b84c9
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2021-11-17 12:43:26 -0500

    Update setlinks.sh

--- setlinks.sh
+++ setlinks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd ~
 #set symbolic links for ease of use
 ln -s ~/keptn-in-a-box/resources/keptn/triggers.sh trigger.sh
 ln -s ~/keptn-in-a-box/resources/gitea/update-git-keptn-post-flight.sh update-git-keptn-post-flight.sh
