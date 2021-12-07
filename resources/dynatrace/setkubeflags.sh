09fb8147882d433d2a8bd4ee0a9fec8246ee0380
Jeff Yarbrough <pcjeffmac@gmail.com>
2021-12-07 18:43:59 -0500

    fix

--- resources/dynatrace/setkubeflags.sh
+++ resources/dynatrace/setkubeflags.sh
@@ -56,10 +56,9 @@ function changeConfig() {
     if [ -f "$CONFIG_FILE" ]; then
         echo "Reading config from file $CONFIG_FILE"
         #TOKENJSON=$(cat $TOKEN_FILE)
-        CONFIG="$(jq '.eventsIntegrationEnabled = true' | \
+        CONFIG=$(echo $CONFIG_FILE | jq '.eventsIntegrationEnabled = true' | \
         jq '.workloadIntegrationEnabled = true' | \
-        jq '.davisEventsIntegrationEnabled = true' $CONFIG_FILE)" && \
-        echo "${CONFIG}" > $CONFIG_FILE
+        jq '.davisEventsIntegrationEnabled = true')
 
     fi
 
