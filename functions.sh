a03de7bba387458c48d229912f881b5ce94e7339
Jeff Yarbrough <pcjeffmac@gmail.com>
2021-12-10 12:30:31 -0500

    fix home path

--- functions.sh
+++ functions.sh
@@ -34,7 +34,8 @@ MICROK8S_CHANNEL="1.19/stable"
 KEPTN_IN_A_BOX_REPO="https://github.com/jyarb-keptn/keptn-in-a-box.git"
 KEPTN_IN_A_BOX_DIR="~/keptn-in-a-box"
 
-USER_KIAB_PATH=$HOME
+USER_HOME_PATH=`echo $HOME`
+USER_KIAB_PATH="$USER_HOME_PATH/keptn-in-a-box"
 
 # - The user to run the commands from. Will be overwritten when executing this shell with sudo, 
 # this is just needed when spinning machines programatically and running the script with root without an interactive shell
