81c15a1af8608c1134e43246484c4071ce47ff57
Jeff Yarbrough <pcjeffmac@gmail.com>
2021-12-17 15:57:00 -0500

    keptnservice try

---
+++ resources/keptnwebservices/deploykeptnwebservice.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ $# -eq 1 ]; then
+    # Read JSON and set it in the CREDS variable 
+    DOMAIN=$1
+    TOKEN=$2
+    echo "Domain has been passed: $DOMAIN"
+else
+    echo "No Domain has been passed, getting it from the Home-Ingress"
+    DOMAIN=$(kubectl get ing -n default homepage-ingress -o=jsonpath='{.spec.tls[0].hosts[0]}')
+    echo "Domain: $DOMAIN"
+fi
+
+sed -e 's~domain.placeholder~'"$DOMAIN"'~' \
+    -e 's~token.placeholder~'"$TOKEN"'~' \
+    templates/deployment.yaml > gen/deployment.yaml
+
+echo "install gitea via Helmchart"
+helm install keptnwebservice keptnwebservice-charts/keptnwebservice -f gen/deployment.yaml --namespace keptn
