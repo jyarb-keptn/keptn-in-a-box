cad4dd945e27c5ff34a701f3a3ee3c8d20b87751
Jeff Yarbrough <pcjeffmac@gmail.com>
2021-12-18 16:40:44 -0500

    cert changes

--- resources/ingress/add-ssl-certificates.sh
+++ resources/ingress/add-ssl-certificates.sh
@@ -5,8 +5,10 @@ echo "reading created ingresses and uncommenting the certmanager and secret"
 for file in gen/*.yaml; do
    yaml=${file##*/}
    echo $file $yaml
-   cat $file | sed -e 's~#cert-manager.io~'cert-manager.io'~' -e 's~#secretName~'secretName'~' -e 's~#acme.cert-manager.io~'acme.cert-manager.io'~' > gen/ssl/$yaml
+   cat $file | sed -e 's~#cert-manager.io~'cert-manager.io'~' -e 's~#secretName~'secretName'~' > gen/ssl/$yaml
 done
+
+#-e 's~#acme.cert-manager.io~'acme.cert-manager.io'~'
 
 echo "Create a certificate for each ingress in the gen/ssl directory"
 kubectl apply -f gen/ssl/
