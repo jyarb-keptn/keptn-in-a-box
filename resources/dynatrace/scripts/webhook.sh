2aa81d0b3ba7cb32f52214c5f9dd4fca1d74cf46
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-01-06 11:52:05 -0500

    token

--- resources/dynatrace/scripts/webhook.sh
+++ resources/dynatrace/scripts/webhook.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-TOKEN=$1
+if [[ -z "$SLACK_TOKEN" ]]; then
+  echo "Is the token set?"
+  TOKEN=$1
+fi
 
 DYNATRACE_TENANT=https://${DT_TENANT}
 DYNATRACE_ENDPOINT=${DYNATRACE_TENANT}/api/config/v1/dashboards
 DYNATRACE_TOKEN=${DT_API_TOKEN}
+TOKEN=${SLACK_TOKEN}
 
 echo "DYNATRACE_ENDPOINT: ${DYNATRACE_ENDPOINT}"
 echo "DYNATRACE_TOKEN: ${DYNATRACE_TOKEN}"
