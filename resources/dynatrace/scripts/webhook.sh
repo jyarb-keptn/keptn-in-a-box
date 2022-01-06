85e8f202c8155f1b2b5fdc2764342261418a1612
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-01-06 08:15:57 -0500

    webhook

---
+++ resources/dynatrace/scripts/webhook.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TOKEN=$1
+
+DYNATRACE_TENANT=https://${DT_TENANT}
+DYNATRACE_ENDPOINT=${DYNATRACE_TENANT}/api/config/v1/dashboards
+DYNATRACE_TOKEN=${DT_API_TOKEN}
+
+echo "DYNATRACE_ENDPOINT: ${DYNATRACE_ENDPOINT}"
+echo "DYNATRACE_TOKEN: ${DYNATRACE_TOKEN}"
+
+KEPTN_ENDPOINT=http://keptn.${KEPTN_DOMAIN}
+
+echo "KEPTN_ENDPOINT: ${KEPTN_ENDPOINT}"
+echo "TOKEN: ${TOKEN}"
+
+export KEPTN_ENDPOINT=${KEPTN_ENDPOINT}
+
+keptn create secret slack-webhook --scope="keptn-webhook-service" --from-literal="token=$TOKEN" --from-literal="bridgeUrl=$KEPTN_ENDPOINT"
