858be5c42cd1db6aa0516cb0142841b4a4c87c65
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-01-12 10:37:53 -0500

    break fix

--- resources/demo/prepfiles.sh
+++ resources/demo/prepfiles.sh
@@ -9,13 +9,13 @@ echo $HOMEDIR
 echo "moving files...."
 
 # copy new format for metric indicators
-cp ${HOMEDIR}/keptn-in-a-box/resources/demo/sli-config-dynatrace.yaml ${HOME}/${KEPTN_EXAMPLES_DIR}/onboarding-carts/sli-config-dynatrace.yaml
+cp ${HOMEDIR}/keptn-in-a-box/resources/demo/sli-config-dynatrace.yaml ${HOMEDIR}/${KEPTN_EXAMPLES_DIR}/onboarding-carts/sli-config-dynatrace.yaml
 
-cp ${HOMEDIR}/${KEPTN_PREP_DIR}/demo_onbording/dynatrace.conf.yaml ${HOME}/${KEPTN_EXAMPLES_DIR}/onboarding-carts/dynatrace.conf.yaml
+cp ${HOMEDIR}/${KEPTN_PREP_DIR}/demo_onbording/dynatrace.conf.yaml ${HOMEDIR}/${KEPTN_EXAMPLES_DIR}/onboarding-carts/dynatrace.conf.yaml
 
 #move files for jmeter
-cp ${HOMEDIR}/${KEPTN_PREP_DIR}/demo_onbording/dev/carts/jmeter/load.jmx ${HOME}/${KEPTN_EXAMPLES_DIR}/onboarding-carts/jmeter/load.jmx
-cp ${HOMEDIR}/${KEPTN_PREP_DIR}/demo_onbording/dev/carts/jmeter/basiccheck.jmx ${HOME}/${KEPTN_EXAMPLES_DIR}/onboarding-carts/jmeter/basiccheck.jmx
+cp ${HOMEDIR}/${KEPTN_PREP_DIR}/demo_onbording/dev/carts/jmeter/load.jmx ${HOMEDIR}/${KEPTN_EXAMPLES_DIR}/onboarding-carts/jmeter/load.jmx
+cp ${HOMEDIR}/${KEPTN_PREP_DIR}/demo_onbording/dev/carts/jmeter/basiccheck.jmx ${HOMEDIR}/${KEPTN_EXAMPLES_DIR}/onboarding-carts/jmeter/basiccheck.jmx
 
 echo "validate port config parameters"
 grep 'PORT' ${HOMEDIR}/${KEPTN_EXAMPLES_DIR}/onboarding-carts/jmeter/basiccheck.jmx
