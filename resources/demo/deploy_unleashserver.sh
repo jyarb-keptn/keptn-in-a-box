53d93479330c4a147d39c628bb185fd2580a1ee6
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-01-19 13:22:06 -0500

    chnage for helm resource

--- resources/demo/deploy_unleashserver.sh
+++ resources/demo/deploy_unleashserver.sh
@@ -9,10 +9,10 @@ if [[ -d "unleash" ]]; then
     
     # The context for this script needs to be in examples/unleash-server
     keptn create project unleash --shipyard=./shipyard.yaml
-    keptn onboard service unleash-db --project=unleash --chart=./unleash-db
-    #keptn add-resource --project=unleash --service=unleash-db --all-stages --resource=./unleash-db --resourceUri=helm/unleash-db
-    keptn onboard service unleash --project=unleash --chart=./unleash
-    #keptn add-resource --project=unleash --service=unleash --all-stages --resource=./unleash --resourceUri=helm/unleash
+    #keptn onboard service unleash-db --project=unleash --chart=./unleash-db
+    keptn add-resource --project=unleash --service=unleash-db --all-stages --resource=./unleash-db --resourceUri=helm/unleash-db
+    #keptn onboard service unleash --project=unleash --chart=./unleash
+    keptn add-resource --project=unleash --service=unleash --all-stages --resource=./unleash --resourceUri=helm/unleash
     keptn trigger delivery --project=unleash --service=unleash-db --image=postgres:10.4
     keptn trigger delivery --project=unleash --service=unleash --image=docker.io/keptnexamples/unleash:1.0.1
 
--- resources/demo/onboard_carts.sh
+++ resources/demo/onboard_carts.sh
@@ -7,8 +7,8 @@ if [[ -d "carts" ]]; then
     keptn add-resource --project=sockshop --resource=./shipyard.yaml --resourceUri=shipyard.yaml
     keptn add-resource --project=sockshop --stage=dev --resource=./shipyard.yaml --resourceUri=shipyard.yaml
     # Onboarding - prepare  Keptn
-    keptn onboard service carts --project=sockshop --chart=./carts
-    #keptn add-resource --project=sockshop --service=carts --all-stages --resource=./carts --resourceUri=helm/carts
+    #keptn onboard service carts --project=sockshop --chart=./carts
+    keptn add-resource --project=sockshop --service=carts --all-stages --resource=./carts --resourceUri=helm/carts
     keptn add-resource --project=sockshop --stage=dev --service=carts --resource=jmeter/basiccheck.jmx --resourceUri=jmeter/basiccheck.jmx
     keptn add-resource --project=sockshop --stage=dev --service=carts --resource=jmeter/load.jmx --resourceUri=jmeter/load.jmx
 
@@ -16,8 +16,8 @@ if [[ -d "carts" ]]; then
     keptn add-resource --project=sockshop --stage=staging --service=carts --resource=jmeter/load.jmx --resourceUri=jmeter/load.jmx
 
     # Onboarding - prepare  Keptn
-    keptn onboard service carts-db --project=sockshop --chart=./carts-db
-    #keptn add-resource --project=sockshop --service=carts-db --all-stages --resource=./carts-db --resourceUri=helm/carts-db 
+    #keptn onboard service carts-db --project=sockshop --chart=./carts-db
+    keptn add-resource --project=sockshop --service=carts-db --all-stages --resource=./carts-db --resourceUri=helm/carts-db 
     #--deployment-strategy=direct
 
 
