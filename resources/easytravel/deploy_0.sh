184634d91feed5ed6b8e953f5ea1b9a9f217959d
Jeff Yarbrough <jeff.yarbrough@dynatrace.com>
2022-02-02 08:41:10 -0500

    Update deploy_0.sh

--- resources/easytravel/deploy_0.sh
+++ resources/easytravel/deploy_0.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -x
 
 # Trigger the deployment
-keptn trigger delivery --project=easytravel --service=easytravel-mongodb --image=docker.io/dynatrace/easytravel-mongodb --sequence=delivery-direct --tag=2.0.0.3349 --labels=creator=cli
+keptn trigger delivery --project=easytravel --service=easytravel-mongodb --image=docker.io/dynatrace/easytravel-mongodb --sequence=delivery-direct --tag=2.0.0.3373 --labels=creator=cli
 sleep 45
 # Trigger the deployment
-keptn trigger delivery --project=easytravel --service=easytravel-backend --image=docker.io/dynatrace/easytravel-backend --tag=2.0.0.3349 --labels=creator=cli
+keptn trigger delivery --project=easytravel --service=easytravel-backend --image=docker.io/dynatrace/easytravel-backend --tag=2.0.0.3373 --labels=creator=cli
 sleep 180
 # Trigger the deployment
-keptn trigger delivery --project=easytravel --service=easytravel-frontend --image=docker.io/dynatrace/easytravel-frontend --tag=2.0.0.3349 --labels=creator=cli
+keptn trigger delivery --project=easytravel --service=easytravel-frontend --image=docker.io/dynatrace/easytravel-frontend --tag=2.0.0.3373 --labels=creator=cli
 sleep 180
 # Trigger the deployment
-keptn trigger delivery --project=easytravel --service=easytravel-www --image=docker.io/dynatrace/easytravel-nginx --tag=2.0.0.3349 --labels=creator=cli
+keptn trigger delivery --project=easytravel --service=easytravel-www --image=docker.io/dynatrace/easytravel-nginx --tag=2.0.0.3373 --labels=creator=cli
 # Trigger the deployment
-keptn trigger delivery --project=easytravel --service=easytravel-angular --image=docker.io/dynatrace/easytravel-angular-frontend --tag=2.0.0.3349 --labels=creator=cli
+keptn trigger delivery --project=easytravel --service=easytravel-angular --image=docker.io/dynatrace/easytravel-angular-frontend --tag=2.0.0.3373 --labels=creator=cli
 
 #sleep 180
 #keptn trigger delivery --project=easytravel --service=loadgenerator --image=docker.io/dynatrace/easytravel-loadgen --sequence=delivery-direct --tag=latest --labels=creator=cli
