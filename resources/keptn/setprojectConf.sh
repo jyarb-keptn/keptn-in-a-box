08dac464b4d4f5e8ccca007900c29a1504c4a137
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-02-18 14:56:35 -0500

    set fix

---
+++ resources/keptn/setprojectConf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~/keptn-in-a-box/resources/keptnwebservices
+
+keptn add-resource --project=performance --service=eval --all-stages --resource=./dynatrace.conf.yaml --resourceUri=dynatrace/dynatrace.conf.yaml
+
+keptn add-resource --project=qualitygate --service=evalservice --all-stages --resource=./dynatrace.conf.yaml --resourceUri=dynatrace/dynatrace.conf.yaml
+
+keptn add-resource --project=performance --resource=./dynatrace.conf.yaml --resourceUri=dynatrace/dynatrace.conf.yaml
+
+keptn add-resource --project=qualitygate --resource=./dynatrace.conf.yaml --resourceUri=dynatrace/dynatrace.conf.yaml
