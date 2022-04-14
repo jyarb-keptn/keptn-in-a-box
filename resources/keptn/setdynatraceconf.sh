50b352accb6400772dd8cb277f193e81b9bcda08
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-04-14 14:51:00 -0400

    project adjustment

--- resources/keptn/setdynatraceconf.sh
+++ resources/keptn/setdynatraceconf.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
     
+    keptn create project dynatrace --shipyard=./dynatrace-qg-shipyard.yaml
    
     keptn add-resource --project=dynatrace --resource=./dynatrace.conf.yaml --resourceUri=dynatrace/dynatrace.conf.yaml
