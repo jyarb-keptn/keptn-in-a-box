936862f8fae0d755ad2dd3bbad6357d5e270bf96
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-04-21 14:53:05 -0400

    use for keptn 0.14.1

--- functions.sh
+++ functions.sh
@@ -8,9 +8,9 @@
 # ==================================================
 #      ----- Components Versions -----             #
 # ==================================================
-KIAB_RELEASE="0.8.12.3"
+KIAB_RELEASE="0.8.12.5"
 # https://github.com/keptn/keptn
-KEPTN_VERSION=0.13.4
+KEPTN_VERSION=0.14.1
 OPERATOR_VERSION=v0.5.0
 ISTIO_VERSION=1.12.6
 CERTMANAGER_VERSION=1.6.1
