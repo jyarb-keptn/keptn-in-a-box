d88a1924086bc064c5f605b189671bb231adcdd5
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-07-13 11:14:34 -0400

    upgrade operator version

--- functions.sh
+++ functions.sh
@@ -11,7 +11,7 @@
 KIAB_RELEASE="0.8.16"
 # https://github.com/keptn/keptn
 KEPTN_VERSION=0.16.1
-OPERATOR_VERSION=v0.6.0
+OPERATOR_VERSION=v0.7.0
 ISTIO_VERSION=1.13.3
 CERTMANAGER_VERSION=1.6.1
 # https://github.com/helm/helm/releases
