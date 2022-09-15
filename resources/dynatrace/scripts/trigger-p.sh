250e16dec3d3020755d4590f9f5642caa48d3d3d
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-09-15 09:22:27 -0400

    added buildid

--- resources/dynatrace/scripts/trigger-p.sh
+++ resources/dynatrace/scripts/trigger-p.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -x
 
 evaldate=`date +%s%3N`
+buildid=`date +%m%d%H%M`
 
-keptn trigger delivery --project=keptnorders --stage=prod-p --service=order --image=dtdemos/dt-orders-order-service:1.0.0 --labels=buildId=1.0.0,version=1.0.0,evaltime=${evaldate},executedBy=manual
+
+keptn trigger delivery --project=keptnorders --stage=prod-p --service=order --image=dtdemos/dt-orders-order-service:1.0.0 --labels=buildId=${buildid},version=1.0.0,evaltime=${evaldate},executedBy=trigger
 rder
-keptn trigger delivery --project=keptnorders --stage=prod-p --service=customer --image=dtdemos/dt-orders-customer-service:1.0.0 --labels=buildId=1.0.0,version=1.0.0,evaltime=${evaldate},executedBy=manual
-keptn trigger delivery --project=keptnorders --stage=prod-p --service=catalog --image=dtdemos/dt-orders-catalog-service:1.0.0 --labels=buildId=1.0.0,version=1.0.0,evaltime=${evaldate},executedBy=manual
-keptn trigger delivery --project=keptnorders --stage=prod-p --service=frontend --image=dtdemos/dt-orders-frontend:1.0.0 --labels=buildId=1.0.0,version=1.0.0,evaltime=${evaldate},executedBy=manual
+keptn trigger delivery --project=keptnorders --stage=prod-p --service=customer --image=dtdemos/dt-orders-customer-service:1.0.0 --labels=buildId=${buildid},version=1.0.0,evaltime=${evaldate},executedBy=trigger
+keptn trigger delivery --project=keptnorders --stage=prod-p --service=catalog --image=dtdemos/dt-orders-catalog-service:1.0.0 --labels=buildId=${buildid},version=1.0.0,evaltime=${evaldate},executedBy=trigger
+keptn trigger delivery --project=keptnorders --stage=prod-p --service=frontend --image=dtdemos/dt-orders-frontend:1.0.0 --labels=buildId=${buildid},version=1.0.0,evaltime=${evaldate},executedBy=trigger
