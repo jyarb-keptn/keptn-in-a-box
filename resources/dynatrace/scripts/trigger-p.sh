3f3257efcad4452e46446737d03c0659b496c5a7
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-09-29 14:12:41 -0400

    trigger order changed

--- resources/dynatrace/scripts/trigger-p.sh
+++ resources/dynatrace/scripts/trigger-p.sh
@@ -3,9 +3,11 @@
 evaldate=`date +%s%3N`
 buildid=`date +%m%d%H%M`
 
-
+keptn trigger delivery --project=keptnorders --stage=prod-p --service=catalog --image=dtdemos/dt-orders-catalog-service:1.0.0 --labels=buildId=${buildid},version=1.0.0,evaltime=${evaldate},executedBy=trigger
+sleep 60
+keptn trigger delivery --project=keptnorders --stage=prod-p --service=customer --image=dtdemos/dt-orders-customer-service:1.0.0 --labels=buildId=${buildid},version=1.0.0,evaltime=${evaldate},executedBy=trigger
+sleep 180
 keptn trigger delivery --project=keptnorders --stage=prod-p --service=order --image=dtdemos/dt-orders-order-service:1.0.0 --labels=buildId=${buildid},version=1.0.0,evaltime=${evaldate},executedBy=trigger
 rder
-keptn trigger delivery --project=keptnorders --stage=prod-p --service=customer --image=dtdemos/dt-orders-customer-service:1.0.0 --labels=buildId=${buildid},version=1.0.0,evaltime=${evaldate},executedBy=trigger
-keptn trigger delivery --project=keptnorders --stage=prod-p --service=catalog --image=dtdemos/dt-orders-catalog-service:1.0.0 --labels=buildId=${buildid},version=1.0.0,evaltime=${evaldate},executedBy=trigger
+sleep 60
 keptn trigger delivery --project=keptnorders --stage=prod-p --service=frontend --image=dtdemos/dt-orders-frontend:1.0.0 --labels=buildId=${buildid},version=1.0.0,evaltime=${evaldate},executedBy=trigger
