5cb352ab4fa766e1c4660a08cf4f78dc93261b86
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-09-29 09:45:13 -0400

    added labels

--- resources/keptn/triggers.sh
+++ resources/keptn/triggers.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=www --timeframe=30m --labels=buildId=3,executedBy=manual
+evaldate=`date +%s%3N`
+buildid=`date +%m%d%H%M`
 
-keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=Auth2 --timeframe=30m --labels=buildId=5,executedBy=manual
+keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=www --timeframe=30m --labels=buildId=${buildid},version=2.0.0.3356,evaltime=${evaldate},executedBy=trigger
 
-keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=check --timeframe=30m --labels=buildId=5,executedBy=manual
+keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=Auth2 --timeframe=30m --labels=buildId=${buildid},version=2.0.0.3356,evaltime=${evaldate},executedBy=trigger
 
-keptn trigger evaluation --project=sockshop --stage=staging --service=carts --timeframe=30m --labels=buildId=5,executedBy=manual
+keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=check --timeframe=30m --labels=buildId=${buildid},version=2.0.0.3356,evaltime=${evaldate},executedBy=trigger
+
+keptn trigger evaluation --project=sockshop --stage=staging --service=carts --timeframe=30m --labels=buildId=${buildid},version=0.12.1,evaltime=${evaldate},executedBy=trigger
