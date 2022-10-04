33a4afdf28680474b428046479dbd8ef307067b9
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-10-04 13:21:09 -0400

    added services

--- resources/keptn/triggers.sh
+++ resources/keptn/triggers.sh
@@ -10,3 +10,13 @@ keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=Auth
 keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=check --timeframe=30m --labels=buildId=${buildid},version=2.0.0.3356,evaltime=${evaldate},executedBy=trigger
 
 keptn trigger evaluation --project=sockshop --stage=staging --service=carts --timeframe=30m --labels=buildId=${buildid},version=0.12.1,evaltime=${evaldate},executedBy=trigger
+
+keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=easytravel-angular --timeframe=30m --labels=buildId=${buildid},version=2.0.0.3356,evaltime=${evaldate},executedBy=trigger
+
+keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=etnginxrest --timeframe=30m --labels=buildId=${buildid},version=2.0.0.3356,evaltime=${evaldate},executedBy=trigger
+
+keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=easytravel-frontend --timeframe=30m --labels=buildId=${buildid},version=2.0.0.3356,evaltime=${evaldate},executedBy=trigger
+
+keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=easytravel-backend --timeframe=30m --labels=buildId=${buildid},version=2.0.0.3356,evaltime=${evaldate},executedBy=trigger
+
+keptn trigger evaluation --project=dynatrace --stage=quality-gate --service=easytravel-www --timeframe=30m --labels=buildId=${buildid},version=2.0.0.3356,evaltime=${evaldate},executedBy=trigger
