73f8fd3791e980f79f7ba4c97619dfdffeaa5765
Jeff Yarbrough <pcjeffmac@gmail.com>
2022-12-19 14:03:41 -0500

    fix

--- functions.sh
+++ functions.sh
@@ -1113,6 +1113,8 @@ loadDynatraceProject() {
     bashas "cd $KEPTN_IN_A_BOX_DIR/resources/keptn && bash $KEPTN_IN_A_BOX_DIR/resources/gitea/git_post_env.sh ${DOMAIN} dynatrace"
     printInfoSection "load dynatrace project"
     bashas "cd $KEPTN_IN_A_BOX_DIR/resources/keptn && bash $KEPTN_IN_A_BOX_DIR/resources/keptn/dynatrace-project.sh"
+    printInfoSection "Create Service for dynatrace project"
+    bashas "cd $KEPTN_IN_A_BOX_DIR/resources/keptn && bash $KEPTN_IN_A_BOX_DIR/resources/keptn/createDynatraceServices.sh"
  fi
 }
 
