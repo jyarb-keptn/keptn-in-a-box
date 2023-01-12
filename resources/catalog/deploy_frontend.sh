e12237bb956c5bc34ab8b62246f59585bd773985
Jeff Yarbrough <pcjeffmac@gmail.com>
2023-01-12 11:33:24 -0500

    addition

---
+++ resources/catalog/deploy_frontend.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+
+evaldate=`date +%s%3N`
+buildid=`date +%m%d%H%M`
+
+# Trigger the deployment
+keptn trigger delivery --project=keptnorders --service=frontend --image=dtdemos/dt-orders-frontend:1.0.0 --labels=buildId=${buildid},version=1.0.0,evaltime=${evaldate},executedBy=cli
