9915e2e97a10d97717a62174f1fa697db65ee904
Jeff Yarbrough <pcjeffmac@gmail.com>
2023-01-13 11:38:17 -0500

    new files

--- resources/catalog/deploy_catalog.sh
+++ resources/catalog/deploy_catalog.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
 
+evaldate=`date +%s%3N`
+buildid=`date +%m%d%H%M`
 # Trigger the deployment
-keptn trigger delivery --project=keptnorders --service=catalog --image=dtdemos/dt-orders-catalog-service:1.0.0
+keptn trigger delivery --project=keptnorders --service=catalog --image=dtdemos/dt-orders-catalog-service:1.0.0 --labels=buildId=${buildid},version=1.0.0,evaltime=${evaldate},executedBy=cli
