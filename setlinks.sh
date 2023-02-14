317a91876729ae93db44b5e9d72781c70a58fc8e
Jeff Yarbrough <pcjeffmac@gmail.com>
2023-02-14 15:22:04 -0500

    added symlink

--- setlinks.sh
+++ setlinks.sh
@@ -3,3 +3,5 @@ cd ~
 #set symbolic links for ease of use
 ln -s ~/keptn-in-a-box/resources/keptn/triggers.sh trigger.sh
 
+ln -s ~/keptn-in-a-box/resources/dynatrace/scripts/trigger-p.sh trigger-p.sh
+
